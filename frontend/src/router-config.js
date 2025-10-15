4f652d94db067041c93384c742465ca2b348f1bf
Mohan7989 <neelamohannadupuru@gmail.com>
2025-10-15 21:48:53 +0530

    Delete frontend directory

--- frontend/src/router-config.js
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom';
-
-export const router = createBrowserRouter([], {
-  future: {
-    v7_startTransition: true,
-    v7_relativeSplatPath: true,
-  },
-});
